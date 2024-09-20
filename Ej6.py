0f0ce64279844551109ac97524edc3b66ca30666
Isaacslrrr <isaac.soler@campus.xaloc.org>
2024-09-20 10:23:20 +0200

    Add files via upload

---
+++ Ej6.py
@@ -0,0 +1,10 @@
+#6. A partir del programa 5. Haz que se muestre por pantalla también la frase en el orden 
+#inverso en que se han introducido las palabras.
+
+var1=input("introduce una palabra :")
+var2=input("ahora otra :")
+var3=input("ahora otra :")
+var4=input("ahora otra :")
+var5=input("ahora otra :")
+
+print(var5,",",var4,",",var3,",",var2,",",var1)
---
+++ Ej7.py
@@ -0,0 +1,19 @@
+#7. programa que calcule dos operandos con los 7 operadores vistos en clase. ¿Cómo puedes 
+#forzar que el resultado de la división tenga 2 decimales?
+
+var1=float(input("Introduce el primer valor :"))
+var2=float(input("Introduce el seguundo valor :"))
+
+var3=var2+var1
+var4=var1-var2
+var5=var1*var2
+var6=(var1/var2)
+var7=var1**var2
+var8=var1//var2
+
+print("la suma es :", var3)
+print("la resta es :", var4)
+print("la multiplicación es :", var5)
+print("la división es :", round(var1/var2, 2))
+print("el exponente es :", var7)
+print("la división entera es :", var8)
