0b88d737fa6b9fa8115c3efc4667f5118ef04d14
Isaacslrrr <isaac.soler@campus.xaloc.org>
2024-11-18 10:11:51 +0100

    Delete Ej8.py

--- Ej8.py
+++
@@ -1,8 +0,0 @@
-#8. programa que pida un número de horas y muestre por pantalla los minutos y segundos
-
-var1=float(input("Introduce un número de horas :"))
-
-var2=var1*60
-var3=var2*60
-
-print("el número de minutos es :", var2, "y en segundos :", var3)
