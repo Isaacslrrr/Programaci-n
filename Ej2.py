30b58488964302e9b90119fc0c81eee9115b7330
Isaacslrrr <isaac.soler@campus.xaloc.org>
2024-11-18 10:10:49 +0100

    Delete Ej2.py

--- Ej2.py
+++
@@ -1,9 +0,0 @@
-#2. Programa que introduzca por teclado tres tipos de variables y se muestren por pantalla
-#en el siguiente orden: número entero, texto y número decimal.
-var1=float(input("introduce un número entero :"))
-var2=input("introduce una letra :")
-var3=float(input("introduce un número decimal :"))
-
-print("El valor entero introducido es :",var1)
-print("La letra introducido es :",var2)
-print("El valor decimal introducido es :",var3)
