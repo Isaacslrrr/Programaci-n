2234e6afbd999734dae362c7c59512688ccdcbbf
Isaacslrrr <isaac.soler@campus.xaloc.org>
2024-11-18 10:10:57 +0100

    Delete Ej3.py

--- Ej3.py
+++
@@ -1,8 +0,0 @@
-#3. Programa que pida dos números enteros y realice la suma correspondiente
-
-var1=float(input("introduce un valor :"))
-var2=float(input("introduce otro valor :"))
-
-var3=var1+var2
-
-print("El resultado de sumar estos dos valores es :", var3)
