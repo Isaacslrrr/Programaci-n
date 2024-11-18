fe03eb9ddb6fb4b643c9e5a6efe4ce2f8f66c788
Isaacslrrr <isaac.soler@campus.xaloc.org>
2024-11-18 10:11:12 +0100

    Delete Ej4.py

--- Ej4.py
+++
@@ -1,7 +0,0 @@
-#4. A partir del código anterior, realiza una versión para números con decimales
-var1=float(input("introduce un valor decimal :"))
-var2=float(input("introduce otro valor decimal :"))
-
-var3=var1+var2
-
-print("El resultado de sumar estos dos valores es :", var3)
