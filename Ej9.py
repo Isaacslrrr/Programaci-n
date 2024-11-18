2b7d07ddd315f69c757b4477438b244516a2066e
Isaacslrrr <isaac.soler@campus.xaloc.org>
2024-11-18 10:12:02 +0100

    Delete Ej9.py

--- Ej9.py
+++
@@ -1,9 +0,0 @@
-#9. programa que pida los segundos y muestre por pantalla y en la misma frase los minutos 
-#y las horas
-
-var1=float(input("Introduce un número de segundos :"))
-
-var2=var1/60
-var3=var2/60
-
-print("el número de minutos es :", var2, "y en horas :", var3)
