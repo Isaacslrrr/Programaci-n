501991dd5c092ace8f342c0e903660641cffd9c5
Isaacslrrr <isaac.soler@campus.xaloc.org>
2025-03-27 09:13:11 +0100

    phayton

---
+++ Ejemplos/ejmplo acumular.py
@@ -0,0 +1,5 @@
+var1=input("¿Cuál es el primer dia de la semana? :")
+var2=input("¿Cuál es el segundo dia de la semana? :")
+var3=input("¿Cuál es el tercer dia de la semana? :")
+
+print("Los dias dichos al reves son :", var3,",", var2,",", var1)
